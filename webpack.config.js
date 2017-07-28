4056c4b083139da18dbdff7ac1c7e114ba62f3a2
matthew fite <mattfite@gmail.com>
2017-07-27 22:14:14 -0700

    Integrate with SSO provider
    - Integrate with Auth0
    - Fixes #9
    - needs to be refactored, but this gets us started

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
     entry: {
         index: './src/index.js',
-        socketio: './src/socketio.js'
+        socketio: './src/socketio.js',
+        app: './src/app.js'
     },
     output: {
         path: path.resolve(__dirname, 'public/js'),
